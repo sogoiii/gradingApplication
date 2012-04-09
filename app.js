2777db93182ce932f922bac8de03b7251f7750bd
Angello Pozo <sogoiii@gmail.com>
2012-04-09 15:42:55 -0700

    Completed the tutorial.
    -completed tutorial
    -i have an idea as to how the whole mongoose thing works
    -need to work a little bit more to completely understand
    
    Signed-off-by: Angello Pozo <sogoiii@gmail.com>

--- app.js
+++ app.js
@@ -12,28 +12,15 @@ var mongoose = require('mongoose')
 var passport = require('passport')
   , LocalStrategy = require('passport-local').Strategy;
 
+var app = module.exports = express.createServer();
 
 //Database
 
-var Schema = mongoose.Schema;  
-
-var Product = new Schema({  
-    title: { type: String, required: true },  
-    description: { type: String, required: true },  
-    style: { type: String, unique: true },  
-    modified: { type: Date, default: Date.now }
-});
-
-var ProductModel = mongoose.model('Product', Product);  
-
-var db = mongoose.connect('mongodb://localhost/soldhere');
-
+mongoose.connect('mongodb://localhost/ecomm_database');
 
 
 
 //extra stuff for testing authentication
-
-
 var users = [
     { id: 1, username: 'bob', password: 'secret', email: 'bob@example.com' }
   , { id: 2, username: 'joe', password: 'birthday', email: 'joe@example.com' }
@@ -103,7 +90,7 @@ passport.use(new LocalStrategy(
 
 
 
-var app = module.exports = express.createServer();
+
 // Configuration
 
 app.configure(function(){
@@ -123,6 +110,7 @@ app.configure(function(){
   app.use(passport.session());
   app.use(app.router);
   app.use(express.static(__dirname + '/public'));
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
 });
 
 /*
@@ -135,22 +123,69 @@ app.configure('production', function(){
 });
 */
 
-//testing code
+
+
+var Schema = mongoose.Schema;  
+
+// SCHEMAS
+
+var Sizes = new Schema({
+    size: { type: String, required: true },
+    available: { type: Number, required: true, min: 0, max: 1000 },
+    sku: { 
+        type: String, 
+        required: true, 
+        validate: [/[a-zA-Z0-9]/, 'Product sku should only have letters and numbers']
+    },
+    price: { type: Number, required: true, min: 0 }
+});
+
+var Images = new Schema({
+    kind: { 
+        type: String, 
+        enum: ['thumbnail', 'catalog', 'detail', 'zoom'],
+        required: true
+    },
+    url: { type: String, required: true }
+});
+
+var Variants = new Schema({
+    color: String,
+    images: [Images],
+    sizes: [Sizes]
+});
+
+var Categories = new Schema({
+    name: String
+});
+
+var Catalogs = new Schema({
+    name: String
+});
+
+//product model
+
+var Product = new Schema({
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    style: { type: String, unique: true },
+    images: [Images],
+    categories: [Categories],
+    catalogs: [Catalogs],
+    variants: [Variants],
+    modified: { type: Date, default: Date.now }
+});
+
+var ProductModel = mongoose.model('Product', Product);  
+
+
+
+
+
+
+
+
 /*
-var users = [
-	{name: 'henry', email: 'henry@email.com' }
-, {name: 'gio', email: 'giovanni@email.com' }
-, {name: 'scott', email: 'isnotalone@email.com' }
-, {name: 'denise', email: 'hadababy@email.com' }
-, {name: 'rej', email: 'isfun@email.com' }
-
-];
-*/
-
-
-
-
-
 // Routes
 app.get('/', routes.index);
 
@@ -169,11 +204,155 @@ app.post('/login',
 
 
 
-
-
 app.get('/users', function(req,res){
 	res.render('users',{users:users, title: 'USERLIST!!!!!'})
 });
+
+*/
+
+
+
+//route for tutorial
+
+//read a list of products
+app.get('/api', function (req, res) {
+  res.send('Ecomm API is running');
+});
+
+// POST to CREATE
+app.post('/api/products', function (req, res) {
+  var product;
+  console.log("POST: ");
+  console.log(req.body);
+  product = new ProductModel({
+    title: req.body.title,
+    description: req.body.description,
+    style: req.body.style,
+    images: req.body.images,
+    categories: req.body.categories,
+    catalogs: req.body.catalogs,
+    variants: req.body.variants
+  });
+  product.save(function (err) {
+    if (!err) {
+      return console.log("created");
+    } else {
+      return console.log(err);
+    }
+  });
+  return res.send(product);
+});
+
+// PUT to UPDATE
+
+// Bulk update
+app.put('/api/products', function (req, res) {
+    var i, len = 0;
+    console.log("is Array req.body.products");
+    console.log(Array.isArray(req.body.products));
+    console.log("PUT: (products)");
+    console.log(req.body.products);
+    if (Array.isArray(req.body.products)) {
+        len = req.body.products.length;
+    }
+    for (i = 0; i < len; i++) {
+        console.log("UPDATE product by id:");
+        for (var id in req.body.products[i]) {
+            console.log(id);
+        }
+        ProductModel.update({ "_id": id }, req.body.products[i][id], function (err, numAffected) {
+            if (err) {
+                console.log("Error on update");
+                console.log(err);
+            } else {
+                console.log("updated num: " + numAffected);
+            }
+        });
+    }
+    return res.send(req.body.products);
+});
+
+// Single update
+app.put('/api/products/:id', function (req, res) {
+  return ProductModel.findById(req.params.id, function (err, product) {
+    product.title = req.body.title;
+    product.description = req.body.description;
+    product.style = req.body.style;
+    product.images = req.body.images;
+    product.categories = req.body.categories;
+    product.catalogs = req.body.catalogs;
+    product.variants = req.body.variants;
+    return product.save(function (err) {
+      if (!err) {
+        console.log("updated");
+      } else {
+        console.log(err);
+      }
+      return res.send(product);
+    });
+  });
+});
+
+// GET to READ
+
+// List products
+app.get('/api/products', function (req, res) {
+  return ProductModel.find(function (err, products) {
+    if (!err) {
+      return res.send(products);
+    } else {
+      return console.log(err);
+    }
+  });
+});
+
+// Single product
+app.get('/api/products/:id', function (req, res) {
+  return ProductModel.findById(req.params.id, function (err, product) {
+    if (!err) {
+      return res.send(product);
+    } else {
+      return console.log(err);
+    }
+  });
+});
+
+// DELETE to DESTROY
+
+// Bulk destroy all products
+app.delete('/api/products', function (req, res) {
+  ProductModel.remove(function (err) {
+    if (!err) {
+      console.log("removed");
+      return res.send('');
+    } else {
+      console.log(err);
+    }
+  });
+});
+
+// remove a single product
+app.delete('/api/products/:id', function (req, res) {
+  return ProductModel.findById(req.params.id, function (err, product) {
+    return product.remove(function (err) {
+      if (!err) {
+        console.log("removed");
+        return res.send('');
+      } else {
+        console.log(err);
+      }
+    });
+  });
+});
+
+
+
+
+
+
+
+
+
 
 app.listen(3000);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
